37f1ac46ed6d5b61949f4f89f0edd70c2e48b9fa
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-05 00:08:16 +0100

    add comment iteration, but its not working 100% correct

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    #binding.pry
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:body))
     @comment.creator = User.first
 
     if @comment.save
       flash[:notice] = "Successfully added a comment!"
-      redirect_to posts_path
+      redirect_to post_path(@post)
     else
       render 'posts/show'
     end
