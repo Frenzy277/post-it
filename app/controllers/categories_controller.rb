96780ca004773f0b5086dbf8b5da05155eeb18d8
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-04 16:30:50 +0100

    add categories controller new create, views, validations, errors, new button in layout

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,18 @@
+class CategoriesController < ApplicationController
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(params.require(:category).permit(:name))
+
+    if @category.save
+      flash[:notice] = "New category has been added successfully!"
+      redirect_to posts_url
+    else
+      render :new
+    end
+  end
+
+end
--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ActiveRecord::Base
   has_many :post_categories, dependent: :destroy
   has_many :posts, through: :post_categories
 
+  validates :name, presence: true, length: { minimum: 5 }
 end
