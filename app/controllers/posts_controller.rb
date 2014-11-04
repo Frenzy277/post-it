192e809fa4be5a5b0cfca89506585ed771412c43
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-04 14:56:16 +0100

    add new,create,edit,update actions, form partial refactoring, edit link to show, new link to index, strong params method, get_post before_action refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show, :edit, :update]
   
   def index
     @posts = Post.all
@@ -7,5 +8,43 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "Your post was created!"
+      redirect_to post_url(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = "Your post was updated!"
+      redirect_to post_url(@post)
+    else
+      render :edit
+    end
+  end
   
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def get_post
+    @post = Post.find(params[:id])
+  end
+
+
 end
