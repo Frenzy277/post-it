e837dee6fde82c251ce849e23978bae13f358a58
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-05 16:20:46 +0100

    add helpers, change category validation, adjust views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 module ApplicationHelper
 
   def fix_url(str)
-    str.start_with?('http://') ? str : "http://#{str}"
+    str.start_with?('http') ? str : "http://#{str}"
+  end
+
+  def pretty_time(time)
+    time.strftime("%m/%d/%Y %I:%M%P %Z ")
   end
 
 end
