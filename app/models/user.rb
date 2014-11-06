0831440994a16096cd23ce859a01b426c21e96aa
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-06 16:47:11 +0100

    update navigation for edit show user and log out

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2 }
-  validates :password, presence: true, length: { minimum: 3 }
+  validates :password, presence: true, on: :create, length: { minimum: 3 }
 
 end
