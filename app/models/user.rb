eb0742bcba1b8955db9991eb6878cca923873304
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-06 14:00:01 +0100

    add bcrypt-ruby, has_secure_password, validations on username, sessions controller starting actions, users controller starting actions, routes users, sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 2 }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/registration', to: 'users#new', as: :register
+
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
   end
   resources :categories, only: [:new, :create, :show]
+  resources :users, only: [:create]
+
 end
