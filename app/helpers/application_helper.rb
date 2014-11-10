c93a77326eb10ee57974797e4184fd3fbe5e68a7
Frenzy277 <tomas.tomecek@hotmail.com>
2014-11-10 10:13:29 +0100

    add default time zone - EST, add user time_zone choice, change pretty_time method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,10 @@ module ApplicationHelper
   end
 
   def pretty_time(time)
-    time.strftime("%m/%d/%Y %I:%M%P %Z ")
+    if logged_in? && !current_user.time_zone.blank?
+      time = time.in_time_zone(current_user.time_zone)
+    end
+    time.strftime("%m/%d/%Y %I:%M%P %Z")
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141109221847) do
+ActiveRecord::Schema.define(version: 20141110085723) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20141109221847) do
     t.string   "password_digest"
     t.string   "slug"
     t.boolean  "admin",           default: false
+    t.string   "time_zone"
   end
 
   add_index "users", ["slug"], name: "index_users_on_slug"
